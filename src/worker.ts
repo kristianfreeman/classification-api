336694095a46cdbdf17d30b8c29ab241881a818b
Kristian Freeman <kristian@kristianfreeman.com>
2024-07-25 09:26:10 -0700

    2024-07-25-09:26:10

--- src/worker.ts
+++ src/worker.ts
@@ -11,6 +11,8 @@ type Classification = {
   text: string
 }
 
+const textUniqError = `D1_ERROR: UNIQUE constraint failed: classifications.text`
+
 const modelName = "@cf/baai/bge-base-en-v1.5"
 
 const app = new Hono<{ Bindings: Bindings }>()
@@ -35,7 +37,10 @@ app.post("/classifications", async c => {
     const record = results[0] as Classification
     const { id, text: recordText } = record
 
-    const { data } = await c.env.AI.run(modelName, { text: [recordText] })
+    const { data } = await c.env.AI.run(
+      modelName,
+      { text: [recordText] }
+    )
     const values = data[0]
 
     await c.env.VECTORIZE_INDEX.upsert([
@@ -44,13 +49,41 @@ app.post("/classifications", async c => {
 
     return c.json({ classification: record })
   } catch (error: any) {
-    const textUniqError = `D1_ERROR: UNIQUE constraint failed: classifications.text`
     if (error.message.includes(textUniqError)) {
       return c.json({ error: "A classification with this text already exists" }, 500)
     } else {
       return c.json({ error }, 500)
     }
   }
+})
+
+app.post("/classify", async c => {
+  const { query } = await c.req.json()
+  if (!query) return c.json({ error: "No query provided" }, 400)
+
+  const { data } = await c.env.AI.run(
+    modelName,
+    { text: [query] }
+  )
+  const values = data[0]
+
+  const { matches } = await c.env.VECTORIZE_INDEX
+    .query(values, {
+      topK: 1
+    })
+
+  const match = matches[0]
+
+  const sqlQuery = "select * from classifications where id = ?1"
+  const classification = await c.env.DB.prepare(sqlQuery)
+    .bind(match.id)
+    .first()
+
+  return c.json({
+    classification,
+    match,
+    query
+  })
 })
 
 app.delete("/classifications/:id", async c => {
@@ -64,28 +97,5 @@ app.delete("/classifications/:id", async c => {
   }
 })
 
-app.post("/classify", async c => {
-  const { query } = await c.req.json()
-  if (!query) return c.json({ error: "No query provided" }, 400)
-
-  const { data } = await c.env.AI.run(modelName, { text: [query] })
-  const values = data[0]
-
-  const { matches } = await c.env.VECTORIZE_INDEX.query(values, {
-    topK: 1
-  })
-
-  const match = matches[0]
-
-  const sqlQuery = "select * from classifications where id = ?1"
-  const classification = await c.env.DB.prepare(sqlQuery).bind(match.id).first()
-
-  return c.json({
-    classification,
-    match,
-    query
-  })
-})
 
 export default app
-
